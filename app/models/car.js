f8b4a508422bb510afdafd5b513d603f044acc2f
Jasdeep Madan <jasdeepm@gmail.com>
2014-12-13 16:34:52 +0530

    added actions to car amd User

--- app/models/car.js
+++ app/models/car.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   	"owner":DS.belongsTo('user',{async:true}),
+  	"capacity":DS.attr('number'),
   	"passengers":DS.hasMany('user',{async:true})
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+	"name":DS.attr('string'),
   "email":DS.attr('string'),
   "src":DS.attr('string'),
   "dest":DS.attr('string'),
